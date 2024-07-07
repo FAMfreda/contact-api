f5783d41a577e1381b333ad9b3912e540dc8c338
FAMfreda <mbordfreda@gmail.com>
2024-07-07 20:28:06 +0000

    my api contacts

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { dbConnection } from "./config/db.js";
 import ContactRouter from "./routes/contact.js";
+import expressOasGenerator from "express-oas-generator"
+import mongoose from "mongoose";
 
 
 // creating a server
 const app = express();
+expressOasGenerator.handleResponses(app, {
+tags:['contact', 'users'],
+mongooseModels:mongoose.modelNames(),
+})
 
 
 //middleware
@@ -16,9 +22,8 @@ dbConnection()
 
 // use the router
 app.use(ContactRouter)
-
-
-
+expressOasGenerator.handleRequests();
+app.use((req, res) => res.redirect('/api-docs/'))
 
 
 const port = process.env.PORT || 8080
